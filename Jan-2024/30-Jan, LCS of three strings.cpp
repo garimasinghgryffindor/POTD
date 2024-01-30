0e18380a6d00625992b1cd40b44f46b26c86e848
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2024-01-30 17:51:12 +0530

    Update 30-Jan, LCS of three strings.cpp

--- Jan-2024/30-Jan, LCS of three strings.cpp
+++ Jan-2024/30-Jan, LCS of three strings.cpp
@@ -77,3 +77,55 @@ class Solution
             return recur(A, B, C, n1, n2, n3);
         }
 };
+
+
+// TABULATION
+class Solution
+{
+    int mem[21][21][21];
+    public:
+        int LCSof3 (string a, string b, string c, int n1, int n2, int n3)
+        {
+            // using tabulation
+            memset(mem, -1, sizeof(mem));
+            
+            for(int i = 0; i < 21; i++) {
+                for(int j = 0; j < 21; j++) {
+                    mem[0][i][j] = 0;
+                    mem[i][0][j] = 0;
+                    mem[i][j][0] = 0;
+                }
+            }
+            
+            for(int i = 1; i <= n1; i++) {
+                for(int j = 1; j <= n2; j++) {
+                    for(int k = 1; k <= n3; k++) {
+                        if(a[i-1] == b[j-1] && b[j-1] == c[k-1]) {
+                             mem[i][j][k] = 1 + mem[i-1][j-1][k-1];
+                        } else {
+                            int temp1 = 0;
+                            if(a[i-1] == b[j-1]) {
+                                temp1 = mem[i][j][k-1];
+                            }
+                            int temp2 = 0;
+                            if(a[i-1] == c[k-1]) {
+                                temp2 = mem[i][j-1][k];
+                            }
+                            int temp3 = 0;
+                            if(b[j-1] == c[k-1]) {
+                                temp3 = mem[i-1][j][k];
+                            }
+                            
+                            int temp4 = mem[i-1][j-1][k];
+                            int temp5 = mem[i][j-1][k-1];
+                            int temp6 = mem[i-1][j][k-1];
+                            
+                            mem[i][j][k] = max({temp1,temp2,temp3,temp4,temp5,temp6});
+                        }
+                    }
+                }
+            }
+            
+            return mem[n1][n2][n3];
+        }
+};

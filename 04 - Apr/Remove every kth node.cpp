8b28f8ce2e519193f613bcfc61ca01e45c8121a0
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2024-04-29 18:26:15 +0530

    Update Remove every kth node.cpp

--- 04 - Apr/Remove every kth node.cpp
+++ 04 - Apr/Remove every kth node.cpp
@@ -1,23 +1,3 @@
-/* Link list Node 
-
-struct Node
-{
-    int data;
-    struct Node* next;
-    
-    Node(int x){
-        data = x;
-        next = NULL;
-    }
-    
-};
-*/
-
-/*You are required to complete this method*/
-
-/* Function to get the middle of the linked list*/
-/*K will always be in range */
-
 class Solution {
     public:
     Node* deleteK(Node *head,int K){

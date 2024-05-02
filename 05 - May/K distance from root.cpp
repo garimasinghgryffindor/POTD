1be33d689b2ae0ab71d59f5307c70f0c1ae6b510
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2024-05-03 00:40:34 +0530

    Create K distance from root.cpp

---
+++ 05 - May/K distance from root.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+    public:
+    // function should print the nodes at k distance from root
+    vector<int> Kdistance(struct Node *root, int k)
+    {
+        vector<int>res;
+        int x = 0;
+        queue<Node*>q;
+        q.push(root);
+        while(!q.empty()) {
+            int sz = q.size();
+            for(int i = 0; i < sz; i++) {
+                Node * tp = q.front();  q.pop();
+                if(x == k) {
+                    res.push_back(tp->data);
+                }
+                if(tp->left) {
+                    q.push(tp->left);
+                }
+                if(tp->right) {
+                    q.push(tp->right);
+                }
+            }
+            
+            if(x == k) break;
+            x++;
+        }
+        
+        return res;
+    }
+};

79ad798e039fde2840082e9411d647ae2db1a4de
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2024-05-23 14:13:15 +0530

    Update 23-May, K-Palindrome.cpp

--- 05 - May/23-May, K-Palindrome.cpp
+++ 05 - May/23-May, K-Palindrome.cpp
@@ -24,3 +24,27 @@ public:
 };
 
 
+// TABULATION
+// User function Template for C++
+
+class Solution{
+public:
+    int kPalindrome(string str, int n, int k)
+    {
+        vector<vector<int>>dp(n, vector<int>(n, 0));
+        
+        for(int len = 2; len <= n; len++) {
+            for(int i = 0; i <= n-len; i++) {
+                int j = i + len - 1;
+                if(str[i] == str[j]) {
+                    dp[i][j] = dp[i+1][j-1];
+                } else {
+                    dp[i][j] = 1 + min(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        int minRemovals = dp[0][n-1];
+        return minRemovals <= k;
+    }
+};

0f351ea60556ddc2bf05d61dd9c56ae6bf83e6c3
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2024-06-09 21:35:39 +0530

    Create 09-Jun, Convert array into Zig-Zag fashion.cpp

---
+++ 06 - Jun/09-Jun, Convert array into Zig-Zag fashion.cpp
@@ -0,0 +1,11 @@
+
+class Solution {
+  public:
+    void zigZag(int n, vector<int> &arr) {
+        for (int i = 0; i < n - 1; ++i) {
+            if ((i % 2 == 0 && arr[i] > arr[i + 1]) || (i % 2 == 1 && arr[i] < arr[i + 1])) {
+                swap(arr[i], arr[i + 1]);
+            }
+        }
+    }
+};

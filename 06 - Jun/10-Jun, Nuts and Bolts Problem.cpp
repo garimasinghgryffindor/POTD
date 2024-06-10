c48470daf538d5e40b91b24f67ef22378591bac1
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2024-06-10 21:32:23 +0530

    Create 10-Jun, Nuts and Bolts Problem.cpp

---
+++ 06 - Jun/10-Jun, Nuts and Bolts Problem.cpp
@@ -0,0 +1,9 @@
+// User function template for C++
+class Solution {
+  public:
+
+    void matchPairs(int n, char nuts[], char bolts[]) {
+        sort(nuts, nuts+n);
+        sort(bolts, bolts+n);
+    }
+};
